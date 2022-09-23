af3632c284c469cac130e05406f2619f9244060e
Stanislav <lazarev@yandex.ru>
2022-09-23 17:53:59 +0300

    finish

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,18 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { gitHubApi } from './GitHub/GitHub.api';
+import { githubReducer } from './GitHub/Github.slice';
 
 export const store = configureStore( {
   reducer: {
-    [gitHubApi.reducerPath]: gitHubApi.reducer
+    [gitHubApi.reducerPath]: gitHubApi.reducer,
+    github: githubReducer
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(gitHubApi.middleware)
 })
 
 
 setupListeners(store.dispatch)
+
+
+export type RootState = ReturnType<typeof store.getState>
